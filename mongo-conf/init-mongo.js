a0a3e129b4e2c81b28e5cb0ba0af29a1177b8b27
徐建国 <xu.jianguo@ikasinfo.com>
2022-05-23 14:25:02 +0800

    mongo配置bug

--- mongo-conf/init-mongo.js
+++ mongo-conf/init-mongo.js
@@ -1,13 +1,4 @@
 
-db.createUser({ user: 'admin', pwd: 'admin123456', roles: [ { role: "root", db: "admin" } ] });
-
-db.auth("admin", "admin123456");
- db.createUser({
- user: 'yapi',
- pwd: '123456',
- roles: [
- { role: "dbAdmin", db: "yapi" },
- { role: "readWrite", db: "yapi" }
- ]
-
- });
+db.createUser({ user: 'admin', pwd: 'ikasinfo123', roles: [ { role: "root", db: "admin" } ] });
+db.auth("admin", "ikasinfo123")
+db.createUser({user: 'yapi', pwd: 'ikasinfo123', roles: [{ role: "dbAdmin", db: "yapi"},{role:"readWrite",db:"yapi"}]})
