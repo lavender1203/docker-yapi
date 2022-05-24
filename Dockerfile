8acf043471746730454b5976d2aac90dabd52b25
徐建国 <xu.jianguo@ikasinfo.com>
2022-05-24 15:18:16 +0800

    修改.gitignore

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:12-alpine
-COPY repositories /etc/apk/repositories
-COPY ["./yapi", "/app/yapi"]
-WORKDIR /app/yapi/vendors 
-RUN npm install --production --registry https://registry.npm.taobao.org
-RUN npm run install-server
-
-EXPOSE 3000 9090
-
--- mongo-conf/init-mongo.js
+++
@@ -1,4 +0,0 @@
-
-db.createUser({ user: 'admin', pwd: 'ikasinfo123', roles: [ { role: "root", db: "admin" } ] });
-db.auth("admin", "ikasinfo123")
-db.createUser({user: 'yapi', pwd: 'ikasinfo123', roles: [{ role: "dbAdmin", db: "yapi"},{role:"readWrite",db:"yapi"}]})
